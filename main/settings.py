1da9c12817b7d3cd490016f57f589f2969bcf83c
SajedeCMaker <sajedetaheri74@gmail.com>
2025-05-29 23:26:42 +0330

    SECURE: Moved SECRET_KEY to .env file

--- main/settings.py
+++ main/settings.py
@@ -1,18 +1,21 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c=r87qo4uy!r2tuvyhk+tzc*=-e*s9&%h=dm3m#&iibt012j)2'
-
+#SECRET_KEY = 'django-insecure-c=r87qo4uy!r2tuvyhk+tzc*=-e*s9&%h=dm3m#&iibt012j)2'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True' 
 
 ALLOWED_HOSTS = []
 USE_THOUSAND_SEPARATOR = True
